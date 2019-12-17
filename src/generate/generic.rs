62f332ecefb8c7d93830fe42fd533e30d5c026d1
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2019-12-17 11:08:28 +0300

    wproxy

--- src/generate/generic.rs
+++ src/generate/generic.rs
@@ -219,6 +219,184 @@ impl<U, REG> W<U, REG> {
     }
 }
 
+///Marker struct for register/field with safe write
+pub struct Safe;
+
+///Marker struct for register/field with unsafe write
+pub struct Unsafe;
+
+///Contains bit offset of field
+pub trait BitOffset {
+    const OFFSET: usize;
+}
+
+/// Marker trait for Enums
+pub trait Variant {}
+
+///Write Proxy
+pub struct WProxy<'a, U, REG, N, FI, O, S>
+where
+    REG: Writable,
+    FI: Writable,
+{
+    w: &'a mut W<U, REG>,
+    _field: marker::PhantomData<(FI, N, O, S)>,
+}
+
+impl<'a, U, REG, N, FI, O, S> WProxy<'a, U, REG, N, FI, O, S>
+where
+    REG: Writable,
+    FI: Writable,
+{
+    pub(crate) fn new(w: &'a mut W<U, REG>) -> Self {
+        Self {
+            w,
+            _field: marker::PhantomData,
+        }
+    }
+}
+
+macro_rules! impl_bit_proxy {
+    ($U:ty) => {
+        impl<'a, REG, FI, O> WProxy<'a, $U, REG, bool, FI, O, Safe>
+        where
+            REG: Writable,
+            FI: Writable + Into<bool>,
+            O: BitOffset,
+        {
+            ///Sets the field bit"
+            #[inline(always)]
+            pub fn set_bit(self) -> &'a mut W<$U, REG> {
+                self.bit(true)
+            }
+            ///Clears the field bit"
+            #[inline(always)]
+            pub fn clear_bit(self) -> &'a mut W<$U, REG> {
+                self.bit(false)
+            }
+            ///Writes raw bits to the field"
+            #[inline(always)]
+            pub fn bit(self, value: bool) -> &'a mut W<$U, REG> {
+                self.w.bits = (self.w.bits & !(0x01 << O::OFFSET)) | (((value as $U) & 0x01) << O::OFFSET);
+                self.w
+            }
+            ///Writes `variant` to the field
+            #[inline(always)]
+            pub fn variant(self, variant: FI) -> &'a mut W<$U, REG> {
+                self.bit(variant.into())
+            }
+        }
+    }
+}
+
+macro_rules! impl_proxy_safe {
+    ($U:ty, $N:ty) => {
+        impl<'a, REG, FI, O> WProxy<'a, $U, REG, $N, FI, O, Safe>
+        where
+            REG: Writable,
+            FI: Writable + Mask<$U>,
+            O: BitOffset,
+        {
+            ///Writes raw bits to the field"
+            #[inline(always)]
+            pub fn bits(self, value: $N) -> &'a mut W<$U, REG> {
+                self.w.bits = (self.w.bits & !(FI::MASK << O::OFFSET)) | (((value as $U) & FI::MASK) << O::OFFSET);
+                self.w
+            }
+        }
+        impl<'a, REG, FI, O> WProxy<'a, $U, REG, $N, FI, O, Safe>
+        where
+            REG: Writable,
+            FI: Writable + Mask<$U> + Into<$N> + Variant,
+            O: BitOffset,
+        {
+            ///Writes `variant` to the field
+            #[inline(always)]
+            pub fn variant(self, variant: FI) -> &'a mut W<$U, REG> {
+                self.bits(variant.into())
+            }
+        }
+    }
+}
+macro_rules! impl_proxy_unsafe {
+    ($U:ty, $N:ty) => {
+        impl<'a, REG, FI, O> WProxy<'a, $U, REG, $N, FI, O, Unsafe>
+        where
+            REG: Writable,
+            FI: Writable + Mask<$U>,
+            O: BitOffset,
+        {
+            ///Writes raw bits to the field"
+            #[inline(always)]
+            pub unsafe fn bits(self, value: $N) -> &'a mut W<$U, REG> {
+                self.w.bits = (self.w.bits & !(FI::MASK << O::OFFSET)) | (((value as $U) & FI::MASK) << O::OFFSET);
+                self.w
+            }
+        }
+        impl<'a, REG, FI, O> WProxy<'a, $U, REG, $N, FI, O, Unsafe>
+        where
+            REG: Writable,
+            FI: Writable + Mask<$U> + Into<$N> + Variant,
+            O: BitOffset,
+        {
+            ///Writes `variant` to the field
+            #[inline(always)]
+            pub fn variant(self, variant: FI) -> &'a mut W<$U, REG> {
+                unsafe { self.bits(variant.into()) }
+            }
+        }
+    }
+}
+
+macro_rules! offsets {
+    ( $( $Ox:ident => $val:literal ),* ) => {
+        $(
+            pub struct $Ox;
+            impl BitOffset for $Ox {
+                const OFFSET: usize = $val;
+            }
+        )*
+    }
+}
+
+offsets!(
+    O0 => 0, O1 => 1, O2 => 2, O3 => 3, O4 => 4,
+    O5 => 5, O6 => 6, O7 => 7, O8 => 8, O9 => 9,
+    O10 => 10, O11 => 11, O12 => 12, O13 => 13, O14 => 14,
+    O15 => 15, O16 => 16, O17 => 17, O18 => 18, O19 => 19,
+    O20 => 20, O21 => 21, O22 => 22, O23 => 23, O24 => 24,
+    O25 => 25, O26 => 26, O27 => 27, O28 => 28, O29 => 29,
+    O30 => 30, O31 => 31, O32 => 32, O33 => 33, O34 => 34,
+    O35 => 35, O36 => 36, O37 => 37, O38 => 38, O39 => 39,
+    O40 => 40, O41 => 41, O42 => 42, O43 => 43, O44 => 44,
+    O45 => 45, O46 => 46, O47 => 47, O48 => 48, O49 => 49,
+    O50 => 50, O51 => 51, O52 => 52, O53 => 53, O54 => 54,
+    O55 => 55, O56 => 56, O57 => 57, O58 => 58, O59 => 59,
+    O60 => 60, O61 => 61, O62 => 62, O63 => 63,
+);
+
+let max_rsize = rsizes.iter().max().unwrap();
+for fsize in &[8, 16, 32, 64] {
+    if fsize > max_rsize {
+        break;
+    }
+    let fty = fsize.to_ty()?;
+    generic_items.push(quote! {
+        impl_bit_proxy!(#fty);
+    });
+}
+
+for (i, rsize) in rsizes.iter().enumerate() {
+    let rty = rsize.to_ty()?;
+    for j in 0..=i {
+        let fty = rsizes[j].to_ty()?;
+        generic_items.push(quote! {
+            impl_proxy_safe!(#rty, #fty);
+            impl_proxy_unsafe!(#rty, #fty);
+        });
+    }
+}
+
 ///Used if enumerated values cover not the whole range
 #[derive(Clone,Copy,PartialEq)]
 pub enum Variant<U, T> {
